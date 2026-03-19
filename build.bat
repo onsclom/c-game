96dbb8c7fb916e55a5cf2e7fec7b055f4e4ec9d2
Austin Merrick <austinmerrick@onsclom.net>
2026-03-18 19:24:12 -0700

    .

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+gcc src\main.c -o game.exe -Ithird-party\raylib\include -Lthird-party\raylib\windows\lib -lraylib -lopengl32 -lgdi32 -lwinmm
+if %errorlevel% neq 0 exit /b %errorlevel%
+game.exe
