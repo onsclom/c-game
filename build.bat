9563b911942a432d72be6633650f5a359cb2a8d3
Austin Merrick <austinmerrick@onsclom.net>
2026-03-22 01:01:22 -0700

    .

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-gcc src\main.c -o game.exe -Ithird-party\raylib\include -Lthird-party\raylib\windows\lib -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc src\main.c -o game.exe -Ithird-party\raylib\include -Lthird-party\raylib\windows\lib -lraylib -lopengl32 -lgdi32 -lwinmm -lm
 if %errorlevel% neq 0 exit /b %errorlevel%
 game.exe
--- src/main.c
+++ src/main.c
@@ -1,18 +1,34 @@
+#include <math.h>
 #include "raylib.h"
 
-int main(void)
-{
-    InitWindow(800, 450, "raylib [core] example - basic window");
+typedef struct {
+  float angle;
+  float target_start;
+} state;
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
+#define WINDOW_WIDTH 400
+#define WINDOW_HEIGHT 400
 
-    CloseWindow();
+int main(void) {
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - basic window");
 
-    return 0;
+  state state = {
+      .angle = 0,
+      .target_start = 0,
+  };
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    state.angle += GetFrameTime();
+    DrawCircle(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, WINDOW_WIDTH/2, BLUE);
+    DrawLine(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, cos((float)state.angle)*WINDOW_WIDTH/2+WINDOW_WIDTH/2, sin(state.angle)*WINDOW_WIDTH/2+WINDOW_WIDTH/2, GREEN);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
 }
